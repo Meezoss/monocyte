42ea2c23874ee763047696a380efd2976c1328c7
meezoss <hamza.04x@gmail.com>
2025-08-12 18:41:45 +0100

    HTML+CSS edit 9.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
+  content: ["./*.html", "./src/**/*.{html,js,ts,jsx,tsx}"],
+  theme: { extend: {} },
   plugins: [],
 }
